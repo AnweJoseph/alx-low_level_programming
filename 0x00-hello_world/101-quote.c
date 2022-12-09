e5e6d1b89c983d36b85852b8411aead9cf343083
AnweJoseph <anweasuquo@yahoo.com>
2022-12-09 02:51:35 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * main - This program is not to use printf or puts
+ * Return: 1
+ *
+*/
+int main(void)
+{
+	write("and that piece of art is useful \" - Dora Korpar, 2015-10-19 \n");
+	return (1);
+}
